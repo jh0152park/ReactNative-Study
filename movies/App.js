b6712d25b5990f3cce270b0ffd0c8aee23e34760
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-27 22:28:01 +0900

    fixed code to splash screen

--- movies/App.js
+++ movies/App.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { WebView } from "react-native-webview";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 
 import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
     const [appIsReady, setAppIsReady] = useState(false);
 
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-
-    async function delay_splash() {
-        await SplashScreen.preventAutoHideAsync();
-        await sleep(3000);
-        await SplashScreen.hideAsync();
-    }
-
     async function startLoading() {
-        await Font.loadAsync(Ionicons.font);
-        await Asset.loadAsync(require("./smile.jpg"));
+        try {
+            await Font.loadAsync(Ionicons.font);
+            await Asset.loadAsync(require("./smile.jpg"));
+        } catch (e) {
+            console.log(`occurred some error: ${e}`);
+        } finally {
+            setAppIsReady(true);
+        }
     }
 
     useEffect(() => {
         startLoading();
-        delay_splash();
     }, []);
 
-    // return <WebView source={{ uri: "https://www.naver.com" }} />;
-    return <Text>Loading Done</Text>;
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) await SplashScreen.hideAsync();
+    }, [appIsReady]);
+
+    if (!appIsReady) return null;
+
+    return (
+        <View onLayout={onLayoutRootView}>
+            <Text>Loading Done</Text>
+        </View>
+    );
 }
