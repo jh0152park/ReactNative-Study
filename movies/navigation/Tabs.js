13c692f9c12f6050f0f13cc3ca1edd50dc4b7012
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-05 00:54:29 +0900

    added tab bar icon

--- movies/navigation/Tabs.js
+++ movies/navigation/Tabs.js
@@ -5,6 +5,7 @@ import TV from "../screens/TV";
 import Search from "../screens/Search";
 import { useColorScheme } from "react-native";
 import { COLOR } from "../colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,23 +14,67 @@ export default function Tabs() {
 
     return (
         <Tab.Navigator
-        // screenOptions={{
-        //     tabBarStyle: {
-        //         backgroundColor: darkMode ? COLOR.BLACK : COLOR.WHITE,
-        //     },
-        //     tabBarActiveTintColor: darkMode ? COLOR.WHITE : COLOR.BLACK,
-        //     tabBarInactiveTintColor: darkMode ? COLOR.GRAY : COLOR.GRAY,
-        //     headerStyle: {
-        //         backgroundColor: darkMode ? COLOR.BLACK : COLOR.WHITE,
-        //     },
-        //     headerTitleStyle: {
-        //         color: darkMode ? COLOR.WHITE : COLOR.BLACK,
-        //     },
-        // }}
+            // screenOptions={{
+            //     tabBarStyle: {
+            //         backgroundColor: darkMode ? COLOR.BLACK : COLOR.WHITE,
+            //     },
+            //     tabBarActiveTintColor: darkMode ? COLOR.WHITE : COLOR.BLACK,
+            //     tabBarInactiveTintColor: darkMode ? COLOR.GRAY : COLOR.GRAY,
+            //     headerStyle: {
+            //         backgroundColor: darkMode ? COLOR.BLACK : COLOR.WHITE,
+            //     },
+            //     headerTitleStyle: {
+            //         color: darkMode ? COLOR.WHITE : COLOR.BLACK,
+            //     },
+            // }}
+            screenOptions={{
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: "bold",
+                },
+            }}
         >
-            <Tab.Screen name="Movies" component={Movies} />
-            <Tab.Screen name="TV" component={TV} />
-            <Tab.Screen name="Search" component={Search} />
+            <Tab.Screen
+                name="Movies"
+                component={Movies}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        return (
+                            <Ionicons
+                                name="film-outline"
+                                color={color}
+                                size={size}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="TV"
+                component={TV}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        return (
+                            <Ionicons
+                                name="tv-outline"
+                                color={color}
+                                size={size}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={Search}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => {
+                        return (
+                            <Ionicons name="search" color={color} size={size} />
+                        );
+                    },
+                }}
+            />
         </Tab.Navigator>
     );
 }
