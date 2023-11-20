b35a366e5911b6e303dc5a5f7061238763d82c15
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-20 23:44:55 +0900

    animated interpolation

--- animations/App.tsx
+++ animations/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Animated, TouchableOpacity} from 'react-native';
+import {Animated, Pressable, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -9,7 +9,7 @@ const Container = styled.View`
 `;
 
 const Box = styled.View`
-  background-color: tomato;
+  background-color: teal;
   width: 200px;
   height: 200px;
 `;
@@ -18,28 +18,34 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 function App() {
   const [up, setUp] = useState(false);
-  const Y = useRef(new Animated.Value(0)).current;
+  const Y_POSITION = useRef(new Animated.Value(250)).current;
+  const opacityValue = Y_POSITION.interpolate({
+    inputRange: [-250, 0, 250],
+    outputRange: [1, 0, 1],
+  });
 
   function toggleUp() {
     setUp(prev => !prev);
   }
 
   function moveUp() {
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 250 : -250,
       useNativeDriver: true,
+      duration: 5000,
     }).start(toggleUp);
   }
 
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{translateY: Y}],
+            opacity: opacityValue,
+            transform: [{translateY: Y_POSITION}],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   );
 }
