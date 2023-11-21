7d9b7c0e19914677fc3b86232a5ed42a7d67ed2b
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-21 17:50:45 +0900

    added tabWidth:4

--- animations/App.tsx
+++ animations/App.tsx
@@ -1,91 +1,92 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Animated, Dimensions, Pressable, TouchableOpacity} from 'react-native';
-import styled from 'styled-components/native';
+import React, {useEffect, useRef, useState} from "react";
+import {Animated, Dimensions, Pressable, TouchableOpacity} from "react-native";
+import styled from "styled-components/native";
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
 `;
 
 const Box = styled.View`
-  background-color: teal;
-  width: 200px;
-  height: 200px;
+    background-color: teal;
+    width: 200px;
+    height: 200px;
 `;
 
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 function App() {
-  const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
+    const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} =
+        Dimensions.get("window");
 
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    }),
-  ).current;
+    const POSITION = useRef(
+        new Animated.ValueXY({
+            x: -SCREEN_WIDTH / 2 + 100,
+            y: -SCREEN_HEIGHT / 2 + 100,
+        }),
+    ).current;
 
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-250, 250],
-    outputRange: ['-360deg', '360deg'],
-  });
+    const rotation = POSITION.y.interpolate({
+        inputRange: [-250, 250],
+        outputRange: ["-360deg", "360deg"],
+    });
 
-  const BGColor = POSITION.y.interpolate({
-    inputRange: [-250, 250],
-    outputRange: ['rgb(246, 250, 45)', 'rgb(83, 255, 71)'],
-  });
+    const BGColor = POSITION.y.interpolate({
+        inputRange: [-250, 250],
+        outputRange: ["rgb(246, 250, 45)", "rgb(83, 255, 71)"],
+    });
 
-  const topLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
+    const topLeft = Animated.timing(POSITION, {
+        toValue: {
+            x: -SCREEN_WIDTH / 2 + 100,
+            y: -SCREEN_HEIGHT / 2 + 100,
+        },
+        useNativeDriver: false,
+    });
 
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
+    const bottomLeft = Animated.timing(POSITION, {
+        toValue: {
+            x: -SCREEN_WIDTH / 2 + 100,
+            y: SCREEN_HEIGHT / 2 - 100,
+        },
+        useNativeDriver: false,
+    });
 
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
+    const bottomRight = Animated.timing(POSITION, {
+        toValue: {
+            x: SCREEN_WIDTH / 2 - 100,
+            y: SCREEN_HEIGHT / 2 - 100,
+        },
+        useNativeDriver: false,
+    });
 
-  const topRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
+    const topRight = Animated.timing(POSITION, {
+        toValue: {
+            x: SCREEN_WIDTH / 2 - 100,
+            y: -SCREEN_HEIGHT / 2 + 100,
+        },
+        useNativeDriver: false,
+    });
 
-  function moveUp() {
-    Animated.loop(
-      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft]),
-    ).start();
-  }
+    function moveUp() {
+        Animated.loop(
+            Animated.sequence([bottomLeft, bottomRight, topRight, topLeft]),
+        ).start();
+    }
 
-  return (
-    <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            backgroundColor: BGColor,
-            transform: [...POSITION.getTranslateTransform()],
-          }}
-        />
-      </Pressable>
-    </Container>
-  );
+    return (
+        <Container>
+            <Pressable onPress={moveUp}>
+                <AnimatedBox
+                    style={{
+                        backgroundColor: BGColor,
+                        transform: [...POSITION.getTranslateTransform()],
+                    }}
+                />
+            </Pressable>
+        </Container>
+    );
 }
 
 export default App;
