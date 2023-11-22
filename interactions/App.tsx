06582801968aa75b39e1b3e165bd06cf9b9603bf
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-22 16:45:00 +0900

    added roate and disappear animation

--- interactions/App.tsx
+++ interactions/App.tsx
@@ -22,45 +22,63 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 
 function App() {
     const scale = useRef(new Animated.Value(1)).current;
+
     const position = useRef(new Animated.Value(0)).current;
+
+    const rotation = position.interpolate({
+        inputRange: [-250, 0, 250],
+        outputRange: ["-30deg", "0deg", "30deg"],
+        extrapolate: "extend",
+    });
+
+    const onPressOut = Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true,
+    });
+
+    const onPressIn = Animated.spring(scale, {
+        toValue: 0.95,
+        useNativeDriver: true,
+    });
+
+    const goCenter = Animated.spring(position, {
+        toValue: 0,
+        useNativeDriver: true,
+    });
 
     const panResponder = useRef(
         PanResponder.create({
             onStartShouldSetPanResponder: () => true,
-            onPanResponderGrant: () => onPressIn(),
-            onPanResponderRelease: () => {
-                onPressOut();
-                Animated.spring(position, {
-                    toValue: 0,
-                    useNativeDriver: true,
-                }).start();
+
+            onPanResponderGrant: () => onPressIn.start(),
+
+            onPanResponderRelease: (_, {dx, dy}) => {
+                if (Math.abs(dx) >= 300) {
+                    Animated.spring(position, {
+                        toValue: dx > 0 ? 500 : -500,
+                        useNativeDriver: true,
+                    }).start();
+                } else {
+                    Animated.parallel([onPressOut, goCenter]).start();
+                }
             },
+
             onPanResponderMove: (_, {dx, dy}) => {
                 position.setValue(dx);
             },
         }),
     ).current;
 
-    function onPressIn() {
-        Animated.spring(scale, {
-            toValue: 0.95,
-            useNativeDriver: true,
-        }).start();
-    }
-
-    function onPressOut() {
-        Animated.spring(scale, {
-            toValue: 1,
-            useNativeDriver: true,
-        }).start();
-    }
-
     return (
         <Container>
             <Card
                 {...panResponder.panHandlers}
                 style={{
-                    transform: [{scale: scale}, {translateX: position}],
+                    transform: [
+                        {scale: scale},
+                        {translateX: position},
+                        {rotateZ: rotation},
+                    ],
                 }}>
                 <Ionicons name="pizza" color={"#192a56"} size={98} />
             </Card>
