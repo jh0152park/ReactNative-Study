1512fd14ea59079f56e1afd80e46312640473f41
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-23 16:32:53 +0900

    diary stack navigator setup

---
+++ diary/App.tsx
@@ -0,0 +1,12 @@
+import {NavigationContainer} from "@react-navigation/native";
+import Navigator from "./navigator";
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Navigator />
+        </NavigationContainer>
+    );
+}
+
+export default App;
---
+++ diary/screens/Home.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import {COLORS} from "../colors";
+
+const View = styled.View`
+    flex: 1;
+    background-color: ${COLORS.BGColor};
+    padding-top: 100px;
+    padding-left: 50px;
+    padding-right: 50px;
+`;
+
+const Title = styled.Text`
+    color: ${COLORS.textColor};
+    font-size: 38px;
+    margin-bottom: 100px;
+`;
+
+const Button = styled.TouchableOpacity`
+    position: absolute;
+    bottom: 50px;
+    right: 20px;
+    background-color: ${COLORS.buttonColor};
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0, 3);
+`;
+
+export default function Home({navigation: {navigate}}: any) {
+    return (
+        <View>
+            <Title>My Diary</Title>
+            <Button onPress={() => navigate("Write")}>
+                <Icon name="add" size={30} color={"white"} />
+            </Button>
+        </View>
+    );
+}
