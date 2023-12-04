855b44945eceadff4988e56228a29829617cfe0a
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-04 21:50:59 +0900

    added price chart

--- crypto/screens/Detail.tsx
+++ crypto/screens/Detail.tsx
@@ -1,25 +1,40 @@
-import React, {useEffect} from "react";
-import {Text} from "react-native";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, Text} from "react-native";
 import styled from "styled-components/native";
 import {Icon} from "../components/Coin";
 import {useQuery} from "react-query";
-import {coinInfo, coinPrices} from "../api";
+import {ICoinPrice, coinInfo, coinPrices} from "../api";
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {Black} from "../colors";
+import {
+    VictoryBar,
+    VictoryChart,
+    VictoryLine,
+    VictoryScatter,
+    VictoryTheme,
+} from "victory-native";
 
-const Container = styled.View``;
+type NaviProps = NativeStackScreenProps<any, "Detail">;
 
-export default function Detail({navigation, route}: any) {
-    const coinId = route.params.item.id;
-    const symbol = route.params.item.symbol;
+const Container = styled.View`
+    flex: 1;
+    background-color: ${Black};
+    justify-content: center;
+    align-items: center;
+`;
+
+export default function Detail({navigation, route}: NaviProps) {
+    const coinId = route.params?.item.id;
+    const symbol = route.params?.item.symbol;
     const {isLoading: infoLoading, data: infoData} = useQuery(
         ["coin_info", coinId],
         () => coinInfo(coinId),
     );
-    const {isLoading: priceLoading, data: priceData} = useQuery(
+    const {isLoading: priceLoading, data: priceData} = useQuery<ICoinPrice[]>(
         ["coin_price", coinId],
         () => coinPrices(coinId),
     );
-
-    console.log(priceData);
+    const [vData, setVData] = useState<any>([]);
 
     useEffect(() => {
         navigation.setOptions({
@@ -33,9 +48,34 @@ export default function Detail({navigation, route}: any) {
         });
     }, []);
 
+    useEffect(() => {
+        if (priceData) {
+            setVData(
+                priceData.map(price => ({
+                    x: new Date(price.timestamp).getTime(),
+                    y: price.price,
+                })),
+            );
+        }
+    }, [priceData]);
+
     return (
         <Container>
-            <Text>DETAIL</Text>
+            {vData ? (
+                <VictoryChart height={360}>
+                    <VictoryLine
+                        data={vData}
+                        style={{data: {stroke: "#1abc9c"}}}
+                        interpolation="monotoneX"
+                    />
+                    <VictoryScatter
+                        data={vData}
+                        style={{data: {fill: "#1abc9c"}}}
+                    />
+                </VictoryChart>
+            ) : (
+                <ActivityIndicator color="white" />
+            )}
         </Container>
     );
 }
